c842613e277b7bbc2da81c0a1442e0ec73a71c18
Noface <166717111+Jerryslang@users.noreply.github.com>
2025-08-11 23:18:23 +0100

    log system rewrite

--- src/log.rs
+++ src/log.rs
@@ -1,48 +1,23 @@
 use atty::Stream;
-use owo_colors::OwoColorize;
 use time::OffsetDateTime;
-use time::macros::format_description; // <- importa el macro
-
-#[derive(Clone, Copy)]
-pub enum ColorKind {
-    Red,
-    Green,
-    Yellow,
-    Blue,
-    Magenta,
-    Cyan,
-    White,
-    Gray,
-}
+use crate::structs::colors;
 
 fn timestamp_str() -> String {
-    let now = OffsetDateTime::now_local().unwrap_or_else(|_| OffsetDateTime::now_utc());
-    let fmt = format_description!("[month]-[day] [hour]:[minute]:[second]");
-    now.format(fmt)
-        .unwrap_or_else(|_| "?".into())
-        .bright_black()
-        .to_string()
+    let now_utc = OffsetDateTime::now_utc();
+    now_utc.unix_timestamp().to_string()
 }
 
-fn colorize(kind: &str, color: ColorKind, use_color: bool) -> String {
+pub fn colorize(kind: &str, color_code: &str, use_color: bool) -> String {
     if !use_color {
-        return format!("[{kind}]");
-    }
-    match color {
-        ColorKind::Red => format!("[{}]", kind.red().bold()),
-        ColorKind::Green => format!("[{}]", kind.green().bold()),
-        ColorKind::Yellow => format!("[{}]", kind.yellow().bold()),
-        ColorKind::Blue => format!("[{}]", kind.blue().bold()),
-        ColorKind::Magenta => format!("[{}]", kind.magenta().bold()),
-        ColorKind::Cyan => format!("[{}]", kind.cyan().bold()),
-        ColorKind::White => format!("[{}]", kind.white().bold()),
-        ColorKind::Gray => format!("[{}]", kind.bright_black().bold()),
+        format!("[{}]", kind)
+    } else {
+        format!("{}[{}]{}", color_code, kind, colors::RESET)
     }
 }
 
-pub fn print_kind(kind: &str, color: ColorKind, msg: String, is_err: bool) {
+pub fn print_kind(kind: &str, color_code: &str, msg: String, is_err: bool) {
     let use_color = atty::is(if is_err { Stream::Stderr } else { Stream::Stdout });
-    let k = colorize(kind, color, use_color);
+    let k = colorize(kind, color_code, use_color);
     let ts = format!("[{}]", timestamp_str());
     if is_err {
         eprintln!("{ts} {k} {msg}");
@@ -52,50 +27,19 @@ pub fn print_kind(kind: &str, color: ColorKind, msg: String, is_err: bool) {
 }
 
 #[macro_export]
-macro_rules! log_gw {
-    ($($arg:tt)*) => {
-        $crate::log::print_kind("GW", $crate::log::ColorKind::Cyan, format!($($arg)*), false);
-    }
-}
+macro_rules! log {
+    ($mode:literal, $($arg:tt)*) => {{
+        let (label, color_code, is_err) = match $mode {
+            "GW"   => ("GW", $crate::structs::colors::CYAN, false),
+            "HB"   => ("HB", $crate::structs::colors::MAGENTA, false),
+            "EVT"  => ("EVT", $crate::structs::colors::BLUE, false),
+            "OK"   => ("OK", $crate::structs::colors::GREEN, false),
+            "WARN" => ("WARN", $crate::structs::colors::YELLOW, true),
+            "ERR"  => ("ERR", $crate::structs::colors::RED, true),
+            "CLI"  => ("CLI", $crate::structs::colors::WHITE, false),
+            _ => ("LOG", $crate::structs::colors::WHITE, false),
+        };
 
-#[macro_export]
-macro_rules! log_hb {
-    ($($arg:tt)*) => {
-        $crate::log::print_kind("HB", $crate::log::ColorKind::Magenta, format!($($arg)*), false);
-    }
-}
-
-#[macro_export]
-macro_rules! log_evt {
-    ($($arg:tt)*) => {
-        $crate::log::print_kind("EVT", $crate::log::ColorKind::Blue, format!($($arg)*), false);
-    }
-}
-
-#[macro_export]
-macro_rules! log_ok {
-    ($($arg:tt)*) => {
-        $crate::log::print_kind("OK", $crate::log::ColorKind::Green, format!($($arg)*), false);
-    }
-}
-
-#[macro_export]
-macro_rules! log_warn {
-    ($($arg:tt)*) => {
-        $crate::log::print_kind("WARN", $crate::log::ColorKind::Yellow, format!($($arg)*), true);
-    }
-}
-
-#[macro_export]
-macro_rules! log_err {
-    ($($arg:tt)*) => {
-        $crate::log::print_kind("ERR", $crate::log::ColorKind::Red, format!($($arg)*), true);
-    }
-}
-
-#[macro_export]
-macro_rules! log_cli {
-    ($($arg:tt)*) => {
-        $crate::log::print_kind("CLI", $crate::log::ColorKind::White, format!($($arg)*), false);
-    }
+        $crate::log::print_kind(label, color_code, format!($($arg)*), is_err)
+    }};
 }
